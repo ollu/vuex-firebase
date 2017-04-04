440f81bee94e52a37b95c3aa18f4684b0d546ec4
Ola Lundén <ola@fickla.nu>
2017-04-04 21:27:42 +0200

    Added store configuration

---
+++ src/store/index.js
@@ -0,0 +1,54 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import Firebase from 'firebase'
+
+Vue.use(Vuex)
+
+let config = {
+  live: {
+    apiKey: "XYZ",
+    authDomain: "YOUR-APP.firebaseapp.com",
+    databaseURL: "https://YOUR-APP.firebaseio.com",
+    projectId: "YOUR-APP",
+    storageBucket: "YOUR-APP.appspot.com",
+    messagingSenderId: "666"
+  },
+  dev: {
+    apiKey: "XYZ",
+    authDomain: "YOUR-APP.firebaseapp.com",
+    databaseURL: "https://YOUR-APP.firebaseio.com",
+    projectId: "YOUR-APP",
+    storageBucket: "YOUR-APP.appspot.com",
+    messagingSenderId: "666"
+  }
+}
+
+let activeConfig = config.dev
+
+if (window.location.host.indexOf('your-domain.nu') !== -1) {
+  activeConfig = config.live
+}
+
+Firebase.initializeApp(activeConfig)
+let db = Firebase.database()
+
+let store = new Vuex.Store({
+  state: {
+    data: '',
+  },
+  mutations: {
+    updateData (state, data) {
+      state.data = data
+    }
+  },
+  actions: {
+    loadData ({ commit }) {
+      let statsRef = db.ref('TABLE-NAME')
+      statsRef.on('value', data => {
+        commit('updateData', data.val())
+      })
+    }
+  }
+})
+
+export default store
